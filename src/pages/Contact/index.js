3aa39338db5a90807ec32187830aa582287d5e61
JuVoros <Julianvoros@hotmail.com>
2021-12-06 18:58:31 -0800

    heroku

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Github from '/Users/juvoros/Code/Bootcamp/homework/react-portfolio/src/images/github.png';
-import Email from '/Users/juvoros/Code/Bootcamp/homework/react-portfolio/src/images/email.png';
-import Linked from '/Users/juvoros/Code/Bootcamp/homework/react-portfolio/src/images/linkedIn.png';
 import './style.css';
 
 const Contact= () => {
